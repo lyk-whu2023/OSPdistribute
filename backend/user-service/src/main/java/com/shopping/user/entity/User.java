ec664958fb447c38603970cecb1014a5e3ad2cc4
lyk_whu <2528075388@qq.com>
2026-03-19 16:04:44 +0800

    部分开发文档

---
+++ backend/user-service/src/main/java/com/shopping/user/entity/User.java
@@ -0,0 +1,28 @@
+package com.shopping.user.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@TableName("users")
+public class User {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+    private String phone;
+    private String email;
+    private String nickname;
+    private String avatar;
+    private Integer gender;
+    private LocalDateTime birthday;
+    private LocalDateTime registerTime;
+    private LocalDateTime lastLoginTime;
+    private Integer status;
+    private String role;
+}
